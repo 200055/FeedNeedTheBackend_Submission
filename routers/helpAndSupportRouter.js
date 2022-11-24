566302fb8829dfd9e41f759dbb2131ad6f33bfa1
Prabesh <dukulog@gmail.com>
2022-11-24 22:27:18 +0545

    edit contact

--- routers/helpAndSupportRouter.js
+++ routers/helpAndSupportRouter.js
@@ -44,6 +44,29 @@ router.get('/contact', async (req,res)=>{
 
 })
 
+// view one contact us 
+router.put('/contact/:id', auth.admin_guard, (req,res)=>{
+    const  _id = req.params.id;
+    const company_name = req.body.company_name;
+    const company_address = req.body.company_address;
+    const company_phone = req.body.company_phone;
+    const company_founded = req.body.company_founded;
+
+        contact.updateOne({
+            _id: _id
+        },{
+            company_name :  company_name,
+            company_address : company_address,
+            company_phone : company_phone,
+            company_founded : company_founded,
+        })
+        .then(()=>{
+            res.json({success:true, msg:"Updated"})}  
+        )
+        .catch((e)=>{
+            res.json({msg:"Failed to update contact us"})
+        })
+})
 
 
 module.exports = router;
