2c0a33874e57c1f4b4a40a83ccc90159f36931a5
Prabesh <dukulog@gmail.com>
2022-11-24 22:26:37 +0545

    add contact us

--- routers/helpAndSupportRouter.js
+++ routers/helpAndSupportRouter.js
@@ -7,6 +7,31 @@ const map = require('../models/mapsModel');
 const contact = require('../models/contactUsModel');
 
 
+// *********contact us*********
+// contact us post
+router.post('/contact',auth.staff_guard, (req,res)=>{
+    const company_name = req.body.company_name;
+    const company_address = req.body.company_address;
+    const company_phone = req.body.company_phone;
+    const company_founded = req.body.company_founded;
+    
+
+    const data = new contact({
+        company_name: company_name,
+        company_address: company_address,
+        company_phone: company_phone,
+        company_founded: company_founded
+    })
+
+    data.save()
+    .then(()=>{
+        res.json({success:true, msg:"Inserted"})}  
+    )
+    .catch((e)=>{
+        res.json({msg:"Failed"})
+    })
+
+})
 
 
 
