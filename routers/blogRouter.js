4300ac80c361540ce42bfaeb1d997485e165dfd2
Prabesh <dukulog@gmail.com>
2022-11-29 22:29:27 +0545

    update blog

--- routers/blogRouter.js
+++ routers/blogRouter.js
@@ -61,5 +61,93 @@ router.get('/blog/:id', async (req,res)=>{
 })
 
 
+// router for updating blog
+router.put('/blog/update/:id', auth.admin_guard,upload.fields([{name:'blog_image',maxCount: 1},{name:'donor_image',maxCount: 1}]) , (req,res)=>{
+    const  _id = req.params.id;
+    const blog_name = req.body.blog_name;
+    const short_desc = req.body.short_desc;
+    const blog_desc = req.body.blog_desc;
+    const blog_category = req.body.blog_category;
+    const donor_name = req.body.donor_name;
+    const blog_price = req.body.blog_price
+    // const blog_category_name = req.body.blog_category_name;
+    // const blog_image = req.file.filename;
+
+    if(req.files['blog_image'] == undefined && req.files['donor_image'] == undefined){
+        blog.updateOne({
+            _id: _id
+        },{
+            blog_name : blog_name,
+            short_desc : short_desc,
+            blog_desc : blog_desc,
+            blog_category : blog_category,
+            donor_name:donor_name,
+            blog_price,blog_price
+            // blog_category_name : blog_category_name,
+        })
+        .then(()=>{
+            res.json({success:true, msg:"Updated"})}  
+        )
+        .catch((e)=>{
+            res.json({msg:"Failed to update blog"})
+        })
+    } else if(req.files['blog_image'] == undefined){
+        blog.updateOne({
+            _id: _id
+        },{
+            blog_name : blog_name,
+            short_desc : short_desc,
+            blog_desc : blog_desc,
+            blog_category : blog_category,
+            donor_name:donor_name,
+            blog_price,blog_price,
+            donor_image: req.files['donor_image'][0].filename
+        })
+        .then(()=>{
+            res.json({success:true, msg:"Updated"})}  
+        )
+        .catch((e)=>{
+            res.json({msg:"Failed to update blog"})
+        })
+    } else if(req.files['donor_image'] == undefined){
+        blog.updateOne({
+            _id: _id
+        },{
+            blog_name : blog_name,
+            short_desc : short_desc,
+            blog_desc : blog_desc,
+            blog_category : blog_category,
+            donor_name:donor_name,
+            blog_price,blog_price,
+            blog_image: req.files['blog_image'][0].filename
+        })
+        .then(()=>{
+            res.json({success:true, msg:"Updated"})}  
+        )
+        .catch((e)=>{
+            res.json({msg:"Failed to update blog"})
+        })
+    }
+    else{
+        blog.updateOne({
+            _id: _id
+        },{
+            blog_name : blog_name,
+            short_desc : short_desc,
+            blog_desc : blog_desc,
+            blog_category : blog_category,
+            donor_name:donor_name,
+            blog_price,blog_price,
+            blog_image: req.files['blog_image'][0].filename,
+            donor_image: req.files['donor_image'][0].filename
+        })
+        .then(()=>{
+            res.json({success:true, msg:"Updated"})}  
+        )
+        .catch((e)=>{
+            res.json({msg:"Failed to update blog"})
+        })
+    }
+})
 
 module.exports = router;
